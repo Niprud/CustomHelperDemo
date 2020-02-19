5d303261b4e567a6a66e1e1e7e056afad8100a2b
Durpin Thapa Magar <niprudthapa@outlook.com>
2020-02-19 13:50:09 +0545

    Add project files.

---
+++ CustomHelper/CustomHelpers/HelperByStaticMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomHelper.CustomHelpers
+{
+    public static class HelperByStaticMethod
+    {
+        /// <summary>  
+        /// This is a Helper Method For Image Element  
+        /// </summary>  
+        /// <param name="Source">Provide Source</param>  
+        /// <param name="Alt">alt Attribute For That Image</param>  
+        /// <param name="Title">Title For the Image</param>  
+        /// <param name="Height">Height Attribute (By Default Height will be 128)</param>  
+        /// <param name="Width">Width Attribute (By Default Width will be 128)</param>  
+        /// <returns></returns>  
+        
+        public static string img(string source, string alt, string title, int height = 128, int width = 128)
+        {
+            return String.Format("<img src='{0}' alt='{1}' title='{2}' height='{3}' width='{4}'/>", source, alt, title, height, width);
+        }
+
+    }
+}
---
+++ CustomHelper/CustomHelpers/HelpersbyExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomHelper.CustomHelpers
+{
+    public static class HelpersbyExt
+    {
+        public static MvcHtmlString img(this HtmlHelper htmlHelper, string Source, string alt, string title, int Height = 128, int Width = 128)
+        {
+            var imgTag = new TagBuilder("image");
+            imgTag.MergeAttribute("src", Source);
+            imgTag.MergeAttribute("alt", alt);
+            imgTag.MergeAttribute("title", title);
+            imgTag.MergeAttribute("width", Width.ToString());
+            imgTag.MergeAttribute("height", Height.ToString());
+
+            return MvcHtmlString.Create(imgTag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
